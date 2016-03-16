e681c6d922de739704877fc7b45aff263e38aeda
Pranay Patil <pranay.patil.dev@gmail.com>
2016-03-16 23:01:11 +0530

    NavigationAdded

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,43 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    buyForm = new BuyForm;
+    sellForm = new SellForm;
+    reportForm = new ReportForm;
+    loginForm = new LogInForm;
+    stack = ui->stackedWidget;
+    stack->addWidget(loginForm);
+    stack->addWidget(buyForm);
+    stack->addWidget(sellForm);
+    stack->addWidget(reportForm);
+    stack->setCurrentWidget(loginForm);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionBuy_triggered()
+{
+    stack->setCurrentWidget(buyForm);
+    ui->actionLog_Out->setText("LogOut");
+}
+
+void MainWindow::on_actionLog_Out_triggered()
+{
+    stack->setCurrentWidget(loginForm);
+    ui->actionLog_Out->setText("Log In");
+}
+
+void MainWindow::on_actionSell_triggered()
+{
+    stack->setCurrentWidget(sellForm);
+    ui->actionLog_Out->setText("LogOut");
+}
+
+void MainWindow::on_actionReport_triggered()
+{
+    stack->setCurrentWidget(reportForm);
+    ui->actionLog_Out->setText("LogOut");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStackedWidget>
+#include "buyform.h"
+#include "sellform.h"
+#include "reportform.h"
+#include "loginform.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +20,22 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionBuy_triggered();
+
+    void on_actionLog_Out_triggered();
+
+    void on_actionSell_triggered();
+
+    void on_actionReport_triggered();
+
 private:
+    QStackedWidget *stack;
     Ui::MainWindow *ui;
+    BuyForm *buyForm;
+    SellForm *sellForm;
+    ReportForm *reportForm;
+    LogInForm *loginForm;
 };
 
 #endif // MAINWINDOW_H
